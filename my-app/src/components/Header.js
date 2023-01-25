8c1b66085c9cda6840aa2c8542c0b022c96e5e53
VABIII <plybyvern6@aol.com>
2023-01-25 10:03:16 -0500

    Changes made

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+    Box,
+    makeStyles,
+    Toolbar,
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    container:{
+        width: "100%"
+    }
+
+}));
+const Header = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
